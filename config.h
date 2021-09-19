6c5077672e48b4e50644a72c8f91afcf096ba950
yesium <yesium@pm.me>
2021-09-19 06:25:44 +0100

    Add new fonts

--- config.h
+++ config.h
@@ -5,7 +5,12 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+static char *font = "Liberation Mono:pixelsize=14:antialias=true:autohint=true:style=bold";
+/* Spare fonts */
+static char *font2[] = {
+	"LiterationMono Nerd Font:pixelsize=14:antialias=true:autohint=true",
+	"JoyPixels:pixelsize=14:antialias=true:autohint=true"
+};
 static int borderpx = 2;
 
 /*
